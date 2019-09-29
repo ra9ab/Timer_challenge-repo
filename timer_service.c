a115e06f8c6815c077c17f8000fb2cc2f5713917
Mohamed Ragab <50461567+ra9ab@users.noreply.github.com>
2019-09-29 02:43:01 +0200

    Update timer_service.c

--- timer_service.c
+++ timer_service.c
@@ -2,16 +2,15 @@
  * sprint_service.c
  *
  *  Created on: 20 Sep 2019
- *      Author: RGB
+ *      Author: Yaakoub
  */
 #include "REG.h"
 #include "macros.h"
 #include "timer_service.h"
+#include "timer.h"
 
 void timer0_delay_ms(uint_32 delay_value)
 {
-	TIMER_TCCR0_REGISTER=0x0B;
-	TIMER_OCR0_REGISTER=125;
 
 	while((delay_value>0))
 	{
@@ -24,28 +23,6 @@ void timer0_delay_ms(uint_32 delay_value)
 	}
 }
 
-/*
-void timer2_delay_ms(uint_32 delay_value)
-
-{
-	TIMER_TCCR2_REGISTER=0x0B;
-	TIMER_OCR0_REGISTER=125;
-
-	while((delay_value>0))
-	{
-		while(get_bit(TIMER_TIFR_REGISTER,7)==0); // 1M
-
-			set_bit(TIMER_TIFR_REGISTER,7);
-			delay_value--;
-
-
-	}
-
-
-}
-*/
-
-
 void timer0_delay_s(uint_32 delay_value)
 {
 
@@ -56,16 +33,6 @@ void timer0_delay_s(uint_32 delay_value)
 	}
 
 }
-/*
-void timer2_delay_s(uint_32 delay_value)
-{
 
-	while(delay_value>0)
-	{
-		timer2_delay_ms(1000);
-	    delay_value--;
-	}
 
-}
 
-*/
